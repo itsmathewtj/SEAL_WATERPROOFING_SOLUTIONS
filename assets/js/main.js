4054cf99709664f21f8de58bca94d762df5439f6
Mathew TJ <mathewtj2112@gmail.com>
2026-03-21 12:32:25 +0530

    first committ

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,18 +37,6 @@
   }
 
   /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
-      }
-    });
-
-  });
-
-  /**
    * Toggle mobile nav dropdowns
    */
   document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
@@ -191,3 +179,14 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+  /**
+   * Hide mobile nav on same-page/hash links
+   */
+  document.querySelectorAll('#navmenu a').forEach(navmenu => {
+    navmenu.addEventListener('click', () => {
+      if (document.querySelector('.mobile-nav-active')) {
+        mobileNavToogle();
+      }
+    });
+
+  });
